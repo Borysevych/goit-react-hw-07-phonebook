6cd260aa453eeba516d9fc09bb4c30a6a87fe880
Borysevych <borysevych.rus@gmail.com>
2023-06-13 22:02:30 +0300

    HW 7 react Final 2.0
    make HW 7 react, new selector

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,35 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux';
 
 import {
-  selectStatusFilter,
-  selectContacts,
   selectIsLoading,
+  selectFilteredContacts,
 } from 'redux/selectors';
 
 import { deleteContact } from 'redux/contactsSlice';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectStatusFilter);
   const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
-
-  const filteredContacts = contacts
-    .filter(contact => {
-      const nameMatch = contact.name
-        ? contact.name.toLowerCase().includes(filter.toLowerCase())
-        : false;
-      const phoneMatch = contact.phone
-        ? contact.phone.toLowerCase().includes(filter.toLowerCase())
-        : false;
-      return nameMatch || phoneMatch;
-    })
-    .sort((a, b) => {
-      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
-      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
-      return 0;
-    });
-
+  const filteredContacts = useSelector(selectFilteredContacts);
+    
   const handleDelete = idToDelete => {
     dispatch(deleteContact(idToDelete));
   };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,20 @@
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
-export const selectStatusFilter = state => state.filter;
+
+
+export const selectFilteredContacts = state => state.contacts.items
+    .filter(contact => {
+      const nameMatch = contact.name
+        ? contact.name.toLowerCase().includes(state.filter.toLowerCase())
+        : false;
+      const phoneMatch = contact.phone
+        ? contact.phone.toLowerCase().includes(state.filter.toLowerCase())
+        : false;
+      return nameMatch || phoneMatch;
+    })
+    .sort((a, b) => {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+      return 0;
+    });
